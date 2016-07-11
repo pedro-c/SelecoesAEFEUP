ab261f9f7f1a9f3274ccc57e373831ff45cb669f
bernardobelchior1 <bernardo.belchior1@gmail.com>
2016-07-11 18:38:50 +0100

    Tweaked how information is shown.

--- www/js/services.js
+++ www/js/services.js
@@ -12,16 +12,22 @@ angular.module('app.services', [])
 
     lastResults = [
         { 
-            team1: 'Andebol Masculino AEFEUP', score1: '1',
-            team2: 'Andebol Masculino AEFMUP', score2: '0'
+            teamType: 'Andebol Masculino',
+            place: 'Pavilhão Desportivo Luís Falcão',
+            team1: 'AEFEUP', score1: '1',
+            team2: 'AEFMUP', score2: '0'
         },
         {
-            team1: 'Futebol 11 Masculino AEFEUP', score1: '3',
-            team2: 'Futebol 11 Masculino AEISEP', score2: '0'
+            teamType: 'Futebol 11 Masculino',
+            place: 'Campo FADEUP',
+            team1: 'AEFEUP', score1: '3',
+            team2: 'AEISEP', score2: '0'
         },
         {
-            team1: 'Voleibol Feminino AEFEUP', score1: '2',
-            team2: 'Voleibol Feminino AEFADEUP', score2: '1'
+            teamType: 'Voleibol Feminino',
+            place: 'Pavilhão Desportivo Luís Falcão',
+            team1: 'AEFEUP', score1: '2',
+            team2: 'AEFADEUP', score2: '1'
         }
     ];
 
@@ -31,22 +37,25 @@ angular.module('app.services', [])
 .factory('NextMatches', [function() {
     nextMatches = [
         {
-            team1: 'Futsal Feminino AEFEUP',
-            team2: 'Futsal Feminino AEFMUP',
+            teamType: 'Futsal Feminino',
+            team1: 'AEFEUP',
+            team2: 'AEFMUP',
             place: 'Pavilhão Desportivo Luís Falcão',
             date: '12/07/2016',
             time: '14h00'
         },
         {
-            team1: 'Rugby 7 AEFEUP',
-            team2: 'Rugby 7 AEFADEUP',
+            teamType: 'Futsal Feminino',
+            team1: 'AEFEUP',
+            team2: 'AEFADEUP',
             place: 'Campo FADEUP',
             date: '13/07/2016',
             time: '17h00'
         },
         {
-            team1: 'Basquetebol Feminino AEFEUP',
-            team2: 'Basquetebol Feminino AEFADEUP',
+            teamType: 'Futsal Feminino',
+            team1: 'AEFEUP',
+            team2: 'AEFADEUP',
             place: 'Pavilhão Desportivo Luís Falcão',
             date: '14/07/2016',
             time: '18h30'
